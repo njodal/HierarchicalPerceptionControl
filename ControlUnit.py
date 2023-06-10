766ffa23af0b75cbf31dd7b150a5068e3f4e7b12
Njodal <jnj@genexus.com>
2023-06-09 21:21:05 -0300

    Primera implementacion funcional del controlador adaptativo.

--- ControlUnit.py
+++ ControlUnit.py
@@ -1,4 +1,5 @@
 from signals import SignalHistory
+import unit_test as ut
 
 
 class PCUControlUnit:
@@ -34,10 +35,12 @@ class AdaptiveControlUnit:
 
     type = 'AdaptiveP'
 
-    def __init__(self, name, gain=1.0, learning_rate=0.01, decay_rate=0.0, past_length=10, debug=False):
+    def __init__(self, name, gain=1.0, learning_rate=0.01, decay_rate=0.0, past_length=10, max_change=0.01,
+                 debug=False):
         self.name          = name
         self.gain          = gain
         self.learning_rate = learning_rate
+        self.max_change    = max_change
         self.decay_rate    = decay_rate
         self.past_length   = past_length
         self.past_errors   = SignalHistory(length=self.past_length)
@@ -58,15 +61,13 @@ class AdaptiveControlUnit:
         self.adjust_weights()
 
         weighted_sum = self.past_errors.weighted_sum(self.weights, lifo_order=True)
-
-        self.o = self.gain*weighted_sum
+        self.o       = self.gain*weighted_sum
 
         if self.debug:
             print('   o:%.3f w_sum:%.3f e:%.3f r:%.3f p:%.3f' % (self.o, weighted_sum, self.e, self.r, self.p))
         return self.o
 
     def adjust_weights(self):
-        # print('values length:%s weight_length:%s' % (len(self.past_errors.values), len(self.weights)))
         for i, e in enumerate(self.past_errors.get_items_in_lifo_order()):
             change = self.learning_rate*self.e*e
             self.weights[i] += change
@@ -76,12 +77,6 @@ class AdaptiveControlUnit:
             if self.debug:
                 print('      i:%s e:%.3f w:%.3f v:%.3f change:%.3f' % (i, e, self.weights[i], self.weights[i]*e,
                                                                        change))
-
-        # for i, e in enumerate(self.past_errors.values):
-        #    self.weights[i] += self.learning_rate*e
-        #    self.weights[i] -= self.weights[i] * self.decay_rate
-        #    if self.debug:
-        #        print('      i:%s e:%.3f w:%.3f v:%.3f' % (i, e, self.weights[i], self.weights[i]*e))
 
 
 def create_control(control_params):
@@ -104,29 +99,39 @@ def create_control(control_params):
 
 
 class DeConvolution:
-    def __init__(self, inputs, outputs, learning_rate=0.01, decay_rate=0.0, past_length=10, debug=False):
-        self.inputs  = inputs
-        self.outputs = outputs
+    def __init__(self, learning_rate=0.01, decay_rate=0.0, past_length=10, max_change=0.01, debug=False):
         self.debug   = debug
         self.o       = 0.0
+        self.error   = 999.0  # any big
         self.past_length   = past_length
         self.learning_rate = learning_rate
+        self.max_change    = max_change
         self.decay_rate    = decay_rate
         self.past_inputs   = SignalHistory(length=self.past_length)
         self.weights       = [0.0 for _ in range(self.past_length+1)]
 
     def get_output(self, input_value, output_value):
+        if self.debug:
+            print('   input:%s output:%s' % (input_value, output_value))
         self.o = self.past_inputs.weighted_sum(self.weights, lifo_order=True)
         self.adjust_weights(input_value, output_value, self.o)
+        new_error = abs(output_value - self.o)
+        if self.debug:
+            improve = 'Good' if new_error < self.error else 'Bad'
+            print('     o:%.3f error:%.4f %s' % (self.o, output_value - self.o, improve))
+        self.error = new_error
         return self.o
 
-    def adjust_weights(self, input_value, actual_output_value, current_output_value):
+    def adjust_weights(self, input_value, actual_output_value, estimated_output_value):
         self.past_inputs.append(input_value)
-        delta_output_value = actual_output_value - current_output_value
+        delta_output_value = actual_output_value - estimated_output_value
         for i, old_input in enumerate(self.past_inputs.get_items_in_lifo_order()):
             change = self.learning_rate*delta_output_value*signum(old_input)
+            if abs(change) > self.max_change:
+                change = self.max_change if change >= 0.0 else - self.max_change
+
             self.weights[i] += change
-            self.weights[i] -= self.weights[i] * self.decay_rate
+            self.weights[i] = self.weights[i]*(1 - self.decay_rate)
             # if self.weights[i] < 0.0:
             #    self.weights[i] = 0.0
             if self.debug:
@@ -140,3 +145,31 @@ def signum(number, min_v=0.000001):
         return 1
     else:
         return 0
+
+
+# Tests
+def test_deconvolution(function_name, max_iter, learning_rate, decay_rate, max_change, past_length, debug):
+    deconvolution = DeConvolution(learning_rate=learning_rate, decay_rate=decay_rate, past_length=past_length,
+                                  max_change=max_change, debug=debug)
+    error = 999.0  # any big
+    for i in range(max_iter):
+        actual_input, actual_output = call_function(function_name, i)
+        estimated_output = deconvolution.get_output(actual_input, actual_output)
+        error = actual_output - estimated_output
+    return error
+
+
+def lineal2(i):
+    return i, i*2
+
+
+def call_function(func_name, i):
+    func = globals().get(func_name)
+    if func is not None and callable(func):
+        return func(i)
+    else:
+        raise Exception("%s is not implemented" % func_name)
+
+
+if __name__ == "__main__":
+    ut.UnitTest(__name__, 'tests/ControlUnit.test', '')
